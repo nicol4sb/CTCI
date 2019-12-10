e0c6898e399ac883a6c097c8fddb4b022fe137d6
nicol4sb <nicolasb@gmail.com>
2019-12-10 06:18:30 +0100

    LinkedList coding challenge

---
+++ Test/src/Test1.java
@@ -0,0 +1,41 @@
+public class Test1 {
+
+	public static void main(String...  args) {
+		LinkedListNode n3 = new Test1.LinkedListNode(null, 3);
+		LinkedListNode n2 = new Test1.LinkedListNode(n3, 2);
+		LinkedListNode n1 = new Test1.LinkedListNode(n2, 1);
+		System.out.println(kthToLast(n1, 3).data);
+	}
+	
+	static class Index{
+		public int value = 0;
+	}
+	
+	static class LinkedListNode{
+		public LinkedListNode next;
+		public Object data;
+		public LinkedListNode(LinkedListNode next, Integer i) {
+			this.data = i;
+			this.next = next;
+					
+		}
+	}
+	
+	public static LinkedListNode kthToLast(LinkedListNode head, int k) {
+		Index idx = new Index();
+		return kthToLast(head, k, idx);
+	}
+
+	private static LinkedListNode kthToLast(LinkedListNode head, int k, Index idx) {
+		if(head == null) {
+			return null;
+		}
+		LinkedListNode node = kthToLast(head.next, k, idx);
+		idx.value = idx.value+1;
+		if(idx.value == k) {
+			return head;
+		}
+		return node;
+	}
+	
+}
